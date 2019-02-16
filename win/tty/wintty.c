2580137715e4280e8e9d182349b1f13ac4fd44e2
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2019-02-16 16:27:32 -0600

    Fixed race selection of races with same first letter.

--- win/tty/wintty.c
+++ win/tty/wintty.c
@@ -1093,6 +1093,7 @@ int role, gend, algn;
     boolean race_ok;
     int i;
     char this_ch;
+    char high_ch;
 
     any = zeroany;
     for (i = 0; races[i].noun; i++) {
@@ -1104,15 +1105,27 @@ int role, gend, algn;
         else
             any.a_string = races[i].noun;
         this_ch = *races[i].noun;
+        high_ch = highc(this_ch);
+        if (this_ch == 'd') {
+            if (!strcmp(races[i].noun, "drow"))
+                this_ch = 'D';
+            else
+                high_ch = 0;
+        } else if (this_ch == 'g') {
+            if (!strcmp(races[i].noun, "ghoul"))
+                this_ch = 'G';
+            else
+                high_ch = 0;
+        }
         /* filtering: picking race, so choose by first letter, with
            capital letter as unseen accelerator;
            !filtering: resetting filter rather than picking, choose by
            capital letter since lowercase role letters will be present */
-        add_menu(win, NO_GLYPH, &any,
-                 filtering ? this_ch : highc(this_ch),
-                 filtering ? highc(this_ch) : 0,
-                 ATR_NONE, races[i].noun,
-                 (!filtering && !race_ok) ? MENU_SELECTED : MENU_UNSELECTED);
+           add_menu(win, NO_GLYPH, &any,
+                    filtering ? this_ch : high_ch,
+                    filtering ? high_ch : 0,
+                    ATR_NONE, races[i].noun,
+                    (!filtering && !race_ok) ? MENU_SELECTED : MENU_UNSELECTED);
     }
 }
 

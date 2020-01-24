2a34b55e2ff30b2090d3bd18b724cdac28797025
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2020-01-23 23:04:32 -0600

    High priest starts with shield of reflection.

--- src/priest.c
+++ src/priest.c
@@ -255,6 +255,7 @@ boolean sanctum; /* is it the seat of the high priest? */
         if (sanctum && EPRI(priest)->shralign == A_NONE
             && on_level(&sanctum_level, &u.uz)) {
             (void) mongets(priest, AMULET_OF_YENDOR);
+            (void) mongets(priest, SHIELD_OF_REFLECTION);
         }
         /* 2 to 4 spellbooks */
         for (cnt = rn1(3, 2); cnt > 0; --cnt) {

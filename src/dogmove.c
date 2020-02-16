d0817ae3b81ec3ecbf15f9c3c26501c8fc779733
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2020-02-16 11:28:24 -0600

    Rewrite fix for pets not attacking peaceful monsters.

--- src/dogmove.c
+++ src/dogmove.c
@@ -1199,10 +1199,9 @@ boolean ranged;
         || mtmp2->data==&mons[PM_PORCUPINE]) && rn2(10)) ||
 		(!ranged &&
 		 max_passive_dmg(mtmp2, mtmp) >= mtmp->mhp) ||
-		(mtmp->mhp*4 < mtmp->mhpmax
+		((mtmp->mhp*4 < mtmp->mhpmax
 		  || mtmp2->data->msound == MS_GUARDIAN
-		  || mtmp2->data->msound == MS_LEADER
-		  || always_peaceful(mtmp2->data)) ||
+		  || mtmp2->data->msound == MS_LEADER) && !Conflict) ||
 		   (!ranged && touch_petrifies(mtmp2->data) &&
 			!resists_ston(mtmp)));
 }

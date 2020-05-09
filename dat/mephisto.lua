142328b85a032be94aaae4629a449328a120a127
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2020-05-09 17:36:10 -0500

    Revamped mephisto and malcanthet levels.
    These were really boring and difficult to explore before. Players deserve better.

--- dat/mephisto.lua
+++ dat/mephisto.lua
@@ -4,72 +4,62 @@ des.level_init({ style = "solidfill", fg = " " });
 
 des.level_flags("mazelevel");
 des.map([[
--------.................................................LLLLLLLLLLLLLLLLLLL
-|.....|......--------------|--------------------------...LLLLLLLLLLLLLLLLLL
-|.....|....................|---|...........|---......|...........LLLLLLLLLL
-|.....|------------........||.................|......|L..........LLLLLLLLLL
-|.....+....................|-...LL.......LL...--.....|LLLLLLLLL..LLLLLLLLLL
----+-----------------------|...LLLL.....LLLL...|.....|LLLLLLLLL......LLLLLL
-..|.|......................|...LLLL.....LLLL...|.....|LLLLLLLLL......LLLLLL
-..|.|.|.|.|---------------.|-...LL.......LL...-|.....|LLLLLLLLLLLLL..LLLLLL
-..|.|.|.|.|..............|.||........L........|......|LLLLLLLLLLLLL..LLLLLL
-..|.|.|.|.|.|-----.......|.|---.....LLL.....---......|...............LLLLLL
-..|.|.|.|.|.|....|..|.|..|.|..|---.......---|........+...............LLLLLL
-..|.|.|.|.|.|..|.|..|.|..|.|.....|.|.|.|.|...........|---------------------
-..|.-----.|.|..--|..|.|..|.|.....|.|.|.|.|...........|..|..................
-..|.......|.|.......|.|..|.|.....|+-----+|...........|..|.|---------------.
-..---------.--------------.|.........|...............|..|.|................
-...........................-+------------------------|..|.|..|.|.|.|.|.|.|.
-...........................|............................-.---------------|.
-.-----------------------------------------------------...................|.
-.....................................................|--------------------.
-...........................................................................
+.......................---------------------------.........................
+...-------.............|.........................|..............-------....
+...|LLLLL|..........---|.LLLLLLLLLLLLLLLLLLLLLLL.|---...........|LLLLL|....
+...|LLLLL|.............|.L.....................L.|..............|LLLLL|....
+...|LLLLL|.............|.L..--------..-------..L.|..............|LLLLL|....
+...|LLLLL|.............|.L..|......|..|.....|..L.|..............|LLLLL|....
+...|LLLLL|......-----..|.L..|......|..|.....|..L.|..-----.......|LLLLL|....
+...---L---......|......|.L..|...............|..L.|......|.......---L---....
+....|LLL|.......|......|.L..|......LLLL.....|..L.|......|........|LLL|.....
+....|LLL|.......|......|.L..|......LLLL.....|..L.|......|........|LLL|.....
+.....LLL........|L.....|.L..|......LLLL.....|..L.|.....L|.........LLL......
+.....LLL........-------|.L..|......LLLL.....|..L.|-------.........LLL......
+......LLL..............|.L..|--...........--|..L.|...............LLL.......
+......LLL..............|.L..|...............|..L.|...............LLL.......
+.....LLLLL.............|.L..-----------------..L.|..............LLLLL......
+.....LLLLL..........---|.LLL.................LLL.|---...........LLLLL......
+...LLL..LLL.....----|LL|.LLLLLLLLLLLLLLLLLLLLLLL.|LL|----......LLL..LLL....
+...LLL...LLLLLLL|LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL|LLLLLLLL...LLL....
+LLLLL...........----|LL---------------------------LL|----............LLLLLL
+LLL.................---LLLLLLLLLLLLLLLLLLLLLLLLLLL---..................LLLL
 ]]);
 des.non_diggable(selection.area(00,00,75,19))
 des.region(selection.area(00,00,75,19), "unlit")
-des.stair("down", 37,05)
-des.stair("up", 04,04)
+des.stair("down", 36,12)
+des.stair("up", 22,00)
+des.door("locked",36,04)
+des.door("locked",37,04)
+des.drawbridge({ dir="east", state="closed", x=15,y=17 });
+des.drawbridge({ dir="west", state="closed", x=57,y=17 });
 --
-des.door("locked",06,04)
-des.door("locked",03,05)
-des.door("locked",53,10)
-des.door("locked",34,13)
-des.door("locked",40,13)
-des.door("locked",28,15)
+des.monster({id="lava demon", x=06, y=04})
+des.monster({id="lava demon", x=67, y=04})
+des.monster("lava demon")
+des.monster("lava demon")
+des.monster("lava demon")
+des.monster("lava demon")
+des.monster("magma elemental")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
+des.monster("&")
 --
-des.monster({class="&", x=07, y=00})
-des.monster({class="&", x=08, y=00})
-des.monster({class="&", x=09, y=00})
-des.monster({class="&", x=10, y=00})
-des.monster({class="&", x=11, y=00})
-des.monster({class="&", x=12, y=00})
-des.monster({class="&", x=07, y=01})
-des.monster({class="&", x=08, y=01})
-des.monster({class="&", x=09, y=01})
-des.monster({class="&", x=10, y=01})
-des.monster({class="&", x=11, y=01})
-des.monster({class="&", x=12, y=01})
-des.monster({class="&", x=07, y=02})
-des.monster({class="&", x=08, y=02})
-des.monster({class="&", x=09, y=02})
-des.monster({class="&", x=10, y=02})
-des.monster({class="&", x=11, y=02})
-des.monster({class="&", x=12, y=02})
---
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster()
-des.monster({id="mephisto", x=37, y=05})
+des.monster({id="pit fiend", x=35, y=12, name="The Left Horn"})
+des.monster({id="pit fiend", x=37, y=12, name="The Right Horn"})
+des.monster({id="hezrou", x=32, y=07})
+des.monster({id="hezrou", x=41, y=07})
+des.monster({id="hezrou", x=33, y=08})
+des.monster({id="hezrou", x=40, y=08})
+des.monster({id="mephisto", x=36, y=12})
 --
 des.object()
 des.object()
@@ -94,10 +84,10 @@ des.trap("land mine")
 des.trap("land mine")
 des.trap("land mine")
 des.trap("land mine")
-des.trap()
-des.trap()
-des.trap()
-des.trap()
+des.trap("fire")
+des.trap("fire")
+des.trap("fire")
+des.trap("fire")
 des.trap()
 des.trap()
 des.trap()

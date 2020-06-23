f2ece3f58916e1b56c91471d082b40d7b22387a6
Michael Meyer <me@entrez.cc>
2020-06-22 23:10:24 -0400

    Use TRUE/FALSE for boolean definition

--- src/spell.c
+++ src/spell.c
@@ -1005,7 +1005,7 @@ boolean atme;
     int otyp, skill, role_skill, res = 0;
     boolean confused = (Confusion != 0);
     boolean physical_damage = FALSE;
-    boolean failed = 0;
+    boolean failed = FALSE;
     struct obj *pseudo;
     struct monst *mtmp;
     coord cc;
@@ -1134,7 +1134,7 @@ boolean atme;
     chance = percent_success(spell);
     if (confused || (rnd(100) > chance)) {
         energy /= 2;
-        failed = 1;
+        failed = TRUE;
     }
 
     /* only can hit this case if using blood magic */

407ccffb4ad96a3d9805b1efdd266bbdbeb1edff
RojjaCebolla <62170514+RojjaCebolla@users.noreply.github.com>
2020-08-13 14:44:37 -0400

    Make dragonbane a lance, and first gift for Knight

--- include/artilist.h
+++ include/artilist.h
@@ -352,9 +352,9 @@ static NEARDATA struct artifact artilist[] = {
       FIRE(5, 0), FIRE(0, 0), NO_CARY, 0, A_NONE, NON_PM, NON_PM, 3000L,
       NO_COLOR),
 
-    A("Dragonbane", BROADSWORD,
+    A("Dragonbane", LANCE,
       (SPFX_RESTR | SPFX_DCLAS | SPFX_REFLECT), 0, S_DRAGON,
-      PHYS(5, 0), NO_DFNS, NO_CARY, 0, A_NONE, NON_PM, NON_PM, 500L,
+      PHYS(5, 0), NO_DFNS, NO_CARY, 0, A_LAWFUL, PM_KNIGHT, NON_PM, 500L,
       NO_COLOR),
 
     A("Demonbane", LONG_SWORD, (SPFX_RESTR | SPFX_WARN | SPFX_DFLAGH), 0, MH_DEMON,

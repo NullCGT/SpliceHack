5c23448f5afab3d921b9eb0752e66365c6c9de8f
RojjaCebolla <62170514+RojjaCebolla@users.noreply.github.com>
2021-08-25 03:02:00 -0400

    Make kicking boots supersede thick_skin/clumsy
    They're magical boots, and the magic is kicking!

--- src/dokick.c
+++ src/dokick.c
@@ -32,9 +32,6 @@ kickdmg(struct monst *mon, boolean clumsy)
     boolean trapkilled = FALSE;
     struct obj* hated_obj = NULL;
 
-    if (uarmf && uarmf->otyp == KICKING_BOOTS)
-        dmg += 5;
-
     /* excessive wt affects dex, so it affects dmg */
     if (clumsy)
         dmg /= 2;
@@ -42,6 +39,9 @@ kickdmg(struct monst *mon, boolean clumsy)
     /* kicking a dragon or an elephant will not harm it */
     if (thick_skinned(mon->data))
         dmg = 0;
+
+    if (uarmf && uarmf->otyp == KICKING_BOOTS)
+        dmg += 5;
 
     /* attacking a shade is normally useless */
     if (mon->data == &mons[PM_SHADE])

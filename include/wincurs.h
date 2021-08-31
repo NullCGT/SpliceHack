cbdc0db72af198cc51bda17ef027f1e830566078
Kestrel Gregorich-Trevor <gregoric@grinnell.edu>
2021-08-30 23:48:50 -0500

    Change curse border color for splice.
    Just in case anyone forgets the wacky variant they are playing.

--- include/wincurs.h
+++ include/wincurs.h
@@ -19,9 +19,9 @@ extern WINDOW *mapwin, *statuswin, *messagewin;    /* Main windows  */
 #define ON 1
 #define NONE -1
 #define KEY_ESC 0x1b
-#define DIALOG_BORDER_COLOR CLR_MAGENTA
+#define DIALOG_BORDER_COLOR CLR_BLUE
 #define ALERT_BORDER_COLOR CLR_RED
-#define SCROLLBAR_COLOR CLR_MAGENTA
+#define SCROLLBAR_COLOR CLR_BLUE
 #define SCROLLBAR_BACK_COLOR CLR_BLACK
 #define HIGHLIGHT_COLOR CLR_WHITE
 #define MORECOLOR CLR_ORANGE

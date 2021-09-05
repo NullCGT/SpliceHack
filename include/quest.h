42ecc00c84e92e6c4876478aa8cb0fbf4e77efe7
Kestrel Gregorich-Trevor <gregoric@grinnell.edu>
2021-09-05 17:29:26 -0500

    Drop min quest level requirement to 10.

--- include/quest.h
+++ include/quest.h
@@ -42,7 +42,6 @@ struct q_score {              /* Quest "scorecard" */
 
 #define MIN_QUEST_ALIGN 20 /* at least this align.record to start */
 /* note: align 20 matches "pious" as reported by enlightenment (cmd.c) */
-#define MIN_QUEST_LEVEL 14 /* at least this u.ulevel to start */
-/* note: exp.lev. 14 is threshold level for 5th rank (class title, role.c) */
+#define MIN_QUEST_LEVEL 10 /* at least this u.ulevel to start */
 
 #endif /* QUEST_H */
